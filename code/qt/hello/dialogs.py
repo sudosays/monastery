0f8639a40aedbb9f659df29639889e8d0ecd9aa1
Julius Stopforth <jjstopforth@gmail.com>
2023-03-15 09:32:20 +0200

    Add notes on qt

---
+++ code/qt/hello/dialogs.py
@@ -0,0 +1,24 @@
+import sys
+from PySide6.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton, QVBoxLayout
+
+
+class Form(QDialog):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        self.setWindowTitle("My Form")
+        self.edit = QLineEdit("Write my name here...")
+        self.button = QPushButton("Show greetings")
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        self.button.clicked.connect(self.greetings)
+
+    def greetings(self):
+        print(f"Hello {self.edit.text()}")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = Form()
+    form.show()
+    sys.exit(app.exec())
---
+++ code/qt/hello/signals.py
@@ -0,0 +1,15 @@
+import sys
+from PySide6.QtWidgets import QApplication, QPushButton
+from PySide6.QtCore import Slot
+
+
+@Slot()
+def say_hello():
+    print("Button clicked, Hello!")
+
+
+app = QApplication([])
+button = QPushButton("Click me")
+button.clicked.connect(say_hello)
+button.show()
+app.exec()
